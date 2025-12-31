4eff1d6e2c36a9a0b8a942e7071e157b85b9c7fa
knigge-lover <bb3210x@icloud.com>
2025-12-31 23:12:23 +0100

    Started implementing chat view

---
+++ MFTalk/ChatsOverviewView.swift
@@ -0,0 +1,61 @@
+//
+//  ChatsView.swift
+//  MFTalk
+//
+//  Created by B on 22/12/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ChatsView: View
+{
+    struct Contact {
+        var username: String
+        var profilepic: Image
+    }
+    
+    @State private var contacts: [Contact] = [
+        Contact(username: "alfred", profilepic: Image(systemName:"globe")),
+        Contact(username: "hans peter", profilepic: Image("NoProfilePic")),
+        Contact(username: "ueli", profilepic: Image("NoProfilePic"))
+    ]
+    
+    // **MAIN UI**
+    var body: some View {
+        NavigationStack {
+            
+            List {
+                
+                ForEach(0..<contacts.count, id: \.self) { i in
+                    HStack {
+                        insertProfilePic(image: contacts[i].profilepic)
+                        Text(contacts[i].username)
+                        Spacer()
+                        Text("\(contacts.count)")
+                    }
+                }
+                .onDelete(perform: delContact)
+            }
+            .navigationTitle("MFTalk")
+        }
+    }
+    
+    // **FUNCTIONS**
+    
+    func delContact(at offsets: IndexSet) {
+        contacts.remove(atOffsets: offsets)
+    }
+    
+    func insertProfilePic(image: Image) -> some View {
+        image
+            .resizable()
+            .frame(width: 45, height: 45, alignment: .topLeading)
+            .clipShape(.circle)
+            .overlay {
+                Circle().stroke(.orange, lineWidth: 2)
+            }
+
+    }
+}
+
